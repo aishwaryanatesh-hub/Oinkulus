7d39030fd0726abc488fdf04b1996b85602123f4
Aishwarya Natesh <aishwarya@frekkel.com>
2026-03-15 14:47:13 +0000

    Initial Commit

---
+++ EMDROverlay/EMDROverlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  EMDROverlayApp.swift
+//  EMDROverlay
+//
+//  Created by Aishwarya Natesh on 15/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct EMDROverlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
